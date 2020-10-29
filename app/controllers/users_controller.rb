f98741f365e2c108b04549b47d8b9c540a0e8e41
adarvin14 <amdarvin14@gmail.com>
2020-10-29 15:27:45 -0400

    create login for users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+Class UsersController < ApplicataionController
+
+    get '/login' do
+        erb :'users/login'
+    end 
+
+    post '/login' do
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect '/library'
+        else 
+            redirect '/signup'
+        end
+    end
+end
