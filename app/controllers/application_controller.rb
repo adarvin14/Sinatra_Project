7f469b4faf53fc26d0202e1a619bbcfa13a03d73
Alex Darvin <amdarvin14@gmail.com>
2020-10-29 19:18:21 +0000

    add code to application controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,33 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+  
+   configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, ENV["SESSION_SECRET"]
+  end
+  
+  get '/' do 
+    erb :welcome
+  end
+
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+    
+    def current_user
+      @user ||= User.find_by(id: session[:user_id]) if logged_in?
+    end
+  end
+  
+  def authenticate      
+      if !logged_in?
+        redirect '/login'
+      end
+    end
+
+end
