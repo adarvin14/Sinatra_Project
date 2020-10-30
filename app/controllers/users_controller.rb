382841076dfc2d25ac9309090d287469247b31b3
adarvin14 <amdarvin14@gmail.com>
2020-10-30 10:55:25 -0400

    signup wip and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,23 @@ class UsersController < ApplicationController
             redirect '/signup'
         end
     end
+
+    get '/signup' do
+        erb :'users/signup'
+    end
+
+    post '/signup' do
+        user = User.create(name: params[:name],
+        email: params[:email],
+        username: params[:username],
+        password: params[:password])
+        
+        #if user is logged in, redirect to library, if not redirect back to login
+
+    end
+
+    get '/logout' do
+        session.destroy
+        redirect '/login'
+    end
 end
