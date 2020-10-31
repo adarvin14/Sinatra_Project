5fbcab158061ce453771cb05ae0e94d9ddc481a5
adarvin14 <amdarvin14@gmail.com>
2020-10-31 13:24:02 -0400

    migrate create_users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201030163324) do
+ActiveRecord::Schema.define(version: 20201031172106) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20201030163324) do
     t.string   "genre"
     t.string   "album"
     t.datetime "release_date"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "username"
+    t.string "password_digest"
   end
 
 end
